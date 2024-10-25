e61698ec90b7fc3fcb91663de08bb6a640a2debb
Alvaro Marmol <amr000.edu@innovasur.com>
2024-10-25 12:33:38 +0200

    Deploy

--- lib/utils/google_sheets.dart
+++ lib/utils/google_sheets.dart
@@ -1,7 +1,7 @@
 class GoogleSheets 
 {
-
-    static const String credenciales = "https://script.google.com/macros/s/AKfycbyPsB_koj3MwkmRFn8IJU-k4sOP8nRfnHHKNNt9xov9INZ1VEsQbu96gDR8Seiz0oDGOQ/exec?spreadsheetId=1qREuUYht73nx_fS2dxm9m6qPs_uvBwsK74dOprmwdjE&sheet=Credenciales";
+    static const String script="AKfycbyPsB_koj3MwkmRFn8IJU-k4sOP8nRfnHHKNNt9xov9INZ1VEsQbu96gDR8Seiz0oDGOQ";
+    static const String credenciales = "https://script.google.com/macros/s/$script/exec?spreadsheetId=1qREuUYht73nx_fS2dxm9m6qPs_uvBwsK74dOprmwdjE&sheet=Credenciales";
 
     static const String cursos = "https://script.google.com/macros/s/AKfycbyPsB_koj3MwkmRFn8IJU-k4sOP8nRfnHHKNNt9xov9INZ1VEsQbu96gDR8Seiz0oDGOQ/exec?spreadsheetId=11Y4M52bYFMCIa5uU52vKll2-OY0VtFiGK2PhMWShngg&sheet=Cursos";
 
